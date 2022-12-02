7e22924fae8fc34f6ebb5f5a8bc7d92d6d81ed73
Stefanomarelli <117829913+5590355@users.noreply.github.com>
2022-12-02 01:40:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
-int main(){
-  return 0;
+int main() {
+    int n,k;
+    bool f;
+
+    do {
+        cout<<"inserisci un numero strettamente positivo"<<endl;
+        cin>>n;
+    }while (n<=0);
+
+    k=n-1;
+    f=numeriprimi (n,k);
+
+    if (f==true) {
+        cout<<"il numero e primo";
+    }
+    else if (f==false) {
+        cout<<"il numero non e primo";
+    }
+
+    return 0;
 }
