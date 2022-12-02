afc4becec196af169b6d5ae7db4700364f2ff74d
Stefanomarelli <117829913+5590355@users.noreply.github.com>
2022-12-02 01:39:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
 
+bool numeriprimi (int n, int k) {
+    if (k==1) {
+        return true;
+    }
+    else if ((n%k)==0) {
+        return false;
+    }
+    else return numeriprimi(n,k-1);
+}
